999c5cde7ba52ebd0e246f1c410340e8c855bd90
Cristhian Canizares <cristhian.canizares@fundacion-jala.org>
2020-10-14 18:46:52 -0400

    Fix errors.

---
+++ json_persistence.py
@@ -0,0 +1,22 @@
+from json import JSONEncoder
+
+
+class Store:
+    def __init__(self, title, answers):
+        self.title = title
+        self.answers = answers
+
+    def save_data(self):
+        data = {}
+
+
+class MyEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+
+store = Store(4, 5)
+json = MyEncoder().encode(store)
+f = open("myform.json", "w")
+f.write(json)
+f.close()
---
+++ quizc/console/quiz_ui_handler.py
@@ -0,0 +1,40 @@
+from json_persistence import Store
+from quizc.console.quiz_input_handler import QuestionInputHandler
+from quizc.console.quiz_ui_menu import QuizUIMenu
+from quizc.model.quiz import Quiz
+from quizc.model.quiz_answers import QuizAnswer, Answer
+
+
+class QuizUIHandler(object):
+
+    @staticmethod
+    def create_quiz() -> Quiz:
+        menu = QuizUIMenu()
+        return menu.handle_create_quiz()
+
+    def fill_quiz(self, quiz) -> QuizAnswer:
+        quiz_answer = QuizAnswer(quiz)
+        question_handler = QuestionInputHandler()
+        print("Quiz:" + quiz.title)
+        for question in quiz.questions:
+            answers = question_handler.ask_question_value(question)
+            answer = Answer(answers, question)
+            quiz_answer.add_answer(answer)
+            self.store_quiz(quiz.title, quiz_answer)
+
+        return quiz_answer
+
+    @staticmethod
+    def store_quiz(title, answer):
+        store = Store(title, answer)
+        store.save_data()
+
+    @staticmethod
+    def show_quiz(quiz_answer):
+        print(quiz_answer.quiz.title)
+        print("=============================================")
+        for answer in quiz_answer.answers:
+            print(answer.question.title)
+            print("ATQ: " + answer.answers[0] + "\n")
+
+        return quiz_answer
